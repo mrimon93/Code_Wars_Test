9dd5a0d490989512a9b08e632fca90466118d4db
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2023-05-15 10:41:25 +0200

    Create Set_Alarm.py

---
+++ Set_Alarm.py
@@ -0,0 +1,6 @@
+def set_alarm(employed, vacation):
+    # The SetAlarm recieves 2 paramaters: Employed and Vaccation
+    # Employed is when you are employed and true when you are in vaccation
+    # The function should return true when you are employed and not on vacation otherwise it should return false
+   
+    return employed and not vacation
