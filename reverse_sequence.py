92e494b754a8b1bc5bb9b9e3f897b8c7e6ec9007
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2023-05-17 11:37:41 +0200

    Create reverse_sequence.py

---
+++ reverse_sequence.py
@@ -0,0 +1,17 @@
+def reverse_seq(n):
+    sequence = []
+    
+    while n > 0:
+        sequence.append(n)
+        n -=1
+    return sequence
+    
+    
+    
+    
+    
+    
+    """
+Build a function that returns an array of integers from n to 1 where n>0.
+Example : n=5 --> [5,4,3,2,1] 
+    """
